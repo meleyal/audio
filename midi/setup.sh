45497341d44617da7f656daf2b1dfb6af60feafd
William Meleyal <william.meleyal@gmail.com>
2026-04-11 21:52:26 -0300

    Remove setup.sh; deps now fully listed in requirements.txt

--- midi/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Install basic-pitch without tensorflow (we use the ONNX backend via onnxruntime)
-pip install basic-pitch --no-deps
-pip install numpy scipy librosa resampy soundfile mir_eval pretty_midi
